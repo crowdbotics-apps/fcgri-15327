d821a0e4f3d24592d1d42d4606e87063becd5960
Crowdbotics <team@crowdbotics.com>
2020-04-02 03:56:55 +0000

    Update models

--- backend/fcgri_15327/wsgi.py
+++ backend/fcgri_15327/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fcgri_15327.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fcgri_15327.settings")
 
 application = get_wsgi_application()
